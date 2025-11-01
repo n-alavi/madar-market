bb7231484869e5dff247656b6b65b8a9612078d7
nargess alavi <alavi_n70@yahoo.com>
2025-11-01 09:57:35 +0330

    add images project

--- packages/app/src/app/components/icons/index.tsx
+++ packages/app/src/app/components/icons/index.tsx
@@ -1,3 +1,53 @@
+export const HomeIcon = ({ className }: { className?: string }) => (
+    <svg
+        className={className}
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path d="M6.66667 9.99996H13.3333M6.66667 13.3333H13.3333M17.5 8.45857V14.9723C17.5 16.8285 16.0076 18.3333 14.1667 18.3333H5.83333C3.99238 18.3333 2.5 16.8285 2.5 14.9723V8.45857C2.5 7.44944 2.9497 6.49371 3.72488 5.8554L7.89155 2.42442C9.11859 1.41403 10.8814 1.41403 12.1084 2.42441L16.2751 5.8554C17.0503 6.49371 17.5 7.44944 17.5 8.45857Z" stroke="#B3B2B2" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+);
 
+export const CartIcon = ({ className }: { className?: string }) => (
+    <svg
+        className={className}
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path d="M2.15569 8.33333H17.8443M15.0293 5L4.97072 5C2.87836 5 1.31329 6.93854 1.73594 9.00666L3.09837 15.6733C3.4149 17.2221 4.76619 18.3333 6.33315 18.3333H13.6669C15.2338 18.3333 16.5851 17.2221 16.9016 15.6733L18.2641 9.00666C18.6867 6.93853 17.1216 5 15.0293 5Z" stroke="#B3B2B2" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M7.5 1.66663L5 4.99996" stroke="#B3B2B2" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M12.5 1.66663L15 4.99996" stroke="#B3B2B2" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M7.5 11.6666L7.5 15" stroke="#B3B2B2" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M12.5 11.6666L12.5 15" stroke="#B3B2B2" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+);
 
+export const OrdersIcon = ({ className }: { className?: string }) => (
+    <svg
+        className={className}
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path d="M14.1667 5.83337L5.83333 5.83337" stroke="#B3B2B2" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M14.1667 9.16663L5.83333 9.16663" stroke="#B3B2B2" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M9.99999 12.5L5.83333 12.5" stroke="#B3B2B2" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M15.8333 1.66663H4.16667C3.24619 1.66663 2.5 2.41282 2.5 3.33329V15.9619C2.5 17.1163 3.6451 17.9211 4.7312 17.5301L6.03267 17.0615C6.43934 16.9151 6.88699 16.9324 7.28115 17.1098L9.31606 18.0255C9.75101 18.2212 10.249 18.2212 10.6839 18.0255L12.7189 17.1098C13.113 16.9324 13.5607 16.9151 13.9673 17.0615L15.2688 17.5301C16.3549 17.9211 17.5 17.1163 17.5 15.9619V3.33329C17.5 2.41282 16.7538 1.66663 15.8333 1.66663Z" stroke="#B3B2B2" stroke-width="1.5" stroke-linecap="round" />
+    </svg>
+);
 
+export const ProfileIcon = ({ className }: { className?: string }) => (
+    <svg
+        className={className}
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+
+        <ellipse cx="10" cy="14.5833" rx="5.83333" ry="2.91667" stroke="#B3B2B2" stroke-width="1.5" stroke-linejoin="round" />
+        <ellipse cx="10" cy="5.83333" rx="3.33333" ry="3.33333" stroke="#B3B2B2" stroke-width="1.5" stroke-linejoin="round" />
+
+    </svg>
+);
